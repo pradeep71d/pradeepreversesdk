2631133f7244aa505d15764ee61bfae1317ca6b0
sandeep <sandeep@ashmar.in>
2019-09-24 11:21:35 +0530

    add

---
+++ app/src/main/java/com/example/pradeepsdk/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pradeepsdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.string1lib.Abc;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ string1lib/src/main/java/com/example/string1lib/Abc.java
@@ -0,0 +1,12 @@
+package com.example.string1lib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Abc {
+  public String show(String s1){
+      String rev=new StringBuffer(s1).reverse().toString();
+      return rev;
+  }
+    }
+
